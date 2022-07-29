8e6d6d4141682c30b66bf2d779c120efa86c8bff
sindhuleaf <109010481+sindhuleaf@users.noreply.github.com>
2022-07-29 23:13:39 +0530

    Add files via upload

---
+++ ReverseName.java
@@ -0,0 +1,13 @@
+package week3.day1;
+
+public class ReverseName {
+
+	public static void main(String[] args) {
+String name="sindhu";
+char[] charArray=name.toCharArray();
+for(int i=charArray.length-1;i>=0;i--)
+	System.out.println(charArray[i]);
+}
+	}
+
+
